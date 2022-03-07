29f60df011d2a9bf7f219b6f63d90a2d3f31c10e
anggadsatria <angga.dsatria27@gmail.com>
2022-03-07 21:19:24 +0000

    Replace ladder.js

--- exercise-3/ladder/ladder.js
+++ exercise-3/ladder/ladder.js
@@ -1,37 +1,18 @@
 function ladder(word) {
     //your code here
     let arr = [];
-    let arr2 = [];
-    let string = [ '4', 'a', 'n', 'g', 'r', 'y', 'm', 'e', 'n' ]
-    for(i=1; i <= string.length;  i++){
-        for(j=0; j <= string.length-1; j++){
-            arr2[i-1][j] = string[i-1];
-            
-            // arr[i-1] += word.toString(word.charAt(j))
-            // arr[i-1] += string.toString[j]
-            // for(k=0 ;k < word.length-j+1; k++){
-            // //    arr[i][j][k] = string[k]
-
-            //     console.log(`array${i-1} index ke ${j} berisi ${string[k]}`)
-            // }
-            // arr += []
-            // arr[i][]
-            // console.log(`index ke  ${i} berisi element ${j}`)
+    for(i=0; i<word.length; i++){
+        let arr2 = [];
+        for(j=0; j < word.length -i; j++){
+            arr2.push(word.charAt(j))
+            // console.log(word.charAt(j))
         }
-        // arr[i-1] = arr2[i-1]
-        // arr2 = [];
-        // console.log(`i =  ${i}`)
+        // console.log(`\n`)
+        arr.push(arr2)
+       
     }
-    // console.log(arr2.length)
-    // console.log(word.length)
-    // for(p=0; p<string.length; p++){
-
-    //     arr[p] = string[p]
-    // }
-    // arr += `${string[0]}`
-    console.log(string.length)
-    return arr2
+    return arr
 }
   
-  // DRIVER CODE
-  console.log(ladder('4angrymen'));
+//   // DRIVER CODE
+console.log(ladder('4angrymen'));
