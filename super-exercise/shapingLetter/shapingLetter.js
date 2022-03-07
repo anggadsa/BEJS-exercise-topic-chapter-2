0ce7ad24becdf6b38e4995e4d81718661c7ff47e
anggadsatria <angga.dsatria27@gmail.com>
2022-03-07 17:54:58 +0700

    add shappinLetter

--- super-exercise/shapingLetter/shapingLetter.js
+++ super-exercise/shapingLetter/shapingLetter.js
@@ -1,21 +1,34 @@
-function shappingLetter(){
-    let num = 5;
-    let string = "";
-
-    for(i=1 ;i <= num; i++){
-
-        for(j=1 ; j <= num ;j++){
-            string += "#"
-        }
-        string += "\n"
+function shappingLetter(num){
     
-        for(k=1 ; k<= num -1 ;k++){
+    let string = "";
+    let ganjil = num / 2 - 0.5;
+    let genap = num / 2 - 0.5
+    // console.log(ganjil) 
+    for(i=1 ;i <= num; i++){
+        if(i == 1 || i == num){
+            for(j=1 ; j <= num ;j++){
+                string += "#"
+            }
+            string += "\n"
+        } else if (num % 2 == 0){
+            for(j=1; j < genap;j++ ){
             string += " "
+            }
+            string += "||"
+            string += "\n"
+        } else {
+            for(j=1; j < ganjil+1;j++ ){
+                string += " "
+            }
+            string += "|"
+            string += "\n"
         }
 
     }
 
-    console.log(string)
+    return string
 }
 
-shappingLetter();
+console.log(shappingLetter(5));
+console.log(shappingLetter(6));
+console.log(shappingLetter(7));
