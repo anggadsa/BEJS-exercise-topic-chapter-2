3ffb6249d063a9921d1e01c0571827dab494757a
anggadsatria <angga.dsatria17@gmail.com>
2022-03-10 23:36:08 +0700

    challenge sekolah.js update

--- challenge-chapter-2/sekolah.js
+++ challenge-chapter-2/sekolah.js
@@ -1,75 +1,51 @@
 var readline = require(`readline-sync`);
 
-class Siswa{
-    constructor(nama, nilai) {
-        this.nama = nama;
-        this.nilai = nilai;
-    }
-}
-
-class Sekolah{
-    constructor(){
-        this.ranking = []; //kumpulan siswa   
-    }
-    newSiswa(nama, nilai){
-        
-        let tambahSiswa = new Siswa(nama, nilai);
-        this.ranking.push(tambahSiswa)
-        return tambahSiswa
-    }
-
-    get semuaSiswa(){
-        return this.ranking;
-    }
-}
-
 let condition = false
+let arr = [];
+
+function ranking(arr){
+    let l = arr.length;
+    let nilai = [];
+    let rataRata = 0;
+    let lulus = 0;
+    let tidakLulus = 0;
+    // Push nilai siswa
+    for(let i=0; i < l; i++){
+        nilai.push(arr[i].nilai);
+    }
+    // Mencari rata rata
+    for (let k=0; k < nilai.length; k++){
+        rataRata = rataRata + nilai[k]
+    }
+    // Jumlah lulus dan tidak lulus
+    for (let j=0; j< nilai.length; j++){
+        if(nilai[j] >= 75){
+            lulus++
+        } else {
+                tidakLulus++
+        }
+    }
+    return `\n1. Nilai tertinggi: ${Math.max(...nilai)} & Nilai Terendah: ${Math.min(...nilai)}\n2. Rata-rata nilai: ${(rataRata/nilai.length).toFixed(1)}\n3. Jumlah Siswa Lulus: ${lulus} & Tidak Lulus: ${tidakLulus}`    
+}
+
+// console.log(ranking(arr));
 
 while(!condition){
-
     let nama = readline.question(`Masukan input 1: `);
     let value = readline.question(`Masukan input 2: `);
 
     let nilai = parseInt(value)
 
-    let inputSiswa = new Sekolah(); //make an object
-    inputSiswa.newSiswa(nama, nilai); //input a siswa
+    let object = {
+        nama : nama,
+        nilai : nilai,
+    } //input a siswa
+    arr.push(object);
 
     let kondisi = readline.question(`Apakah anda ingin lanjut tekan (ya?/q): `);
     // console.log(inputSiswa.semuaSiswa);
     if(kondisi === `q`){
         condition = true
-        console.log(inputSiswa)
-        console.log(inputSiswa.semuaSiswa);
-       
-    
+        console.log(ranking(arr));
     }
 }
-
-
-// while(!condition){
-
-//     let nama = readline.question(`Masukan input 1: `);
-//     let value = readline.question(`Masukan input 2: `);
-
-//     let nilai = parseInt(value)
-
-//     let inputSiswa = new Sekolah(); //make an object
-//     inputSiswa.newSiswa(nama, nilai); //input a siswa
-
-//     let kondisi = readline.question(`Apakah anda ingin lanjut tekan (ya?/q): `);
-//     // console.log(inputSiswa.semuaSiswa);
-//     if(kondisi === `q`){
-//         condition = true
-//         console.log(inputSiswa)
-//         console.log(inputSiswa.semuaSiswa);
-       
-    
-//     }
-// }
-    
-// let inputSiswa1 = new Sekolah();
-// inputSiswa1.newSiswa(`Angga`, 90);
-// inputSiswa1.newSiswa(`Dwi`, 80);
-
-// console.log(inputSiswa1.semuaSiswa)
